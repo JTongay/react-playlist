61e88c026c4848bb8ecb12032f41bcedacf10877
Joseph Tongay <dirk.tongay@gmail.com>
2017-02-28 18:17:14 -0600

    make a folder for nested components with the same name of the parent component

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,9 +1,14 @@
 import React, { PropTypes } from 'react'
 
+import Title from './Header/Title'
+
 const Header = React.createClass({
   render () {
     return (
+    <div>
+      <Title />
       <header>header</header>
+    </div>
     )
   }
 })
---
+++ src/app/components/Header/Title.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react'
+
+const Title = React.createClass({
+  render () {
+    return (
+      <h1>Welcome</h1>
+    )
+  }
+})
+
+export default Title
